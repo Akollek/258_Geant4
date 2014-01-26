bbecf7e0aac6a2a67a9c872f2e92147113ee96ee
Amiel Kollek <amielkollekb@web012040.wireless.mcgill.ca>
2014-01-25 19:16:25 -0500

    Possible base application

---
+++ Desktop/Trendi/Trendi/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Trendi
+//
+//  Created by Amiel Kollek on 14-01-15.
+//  Copyright (c) 2014 Amiel Kollek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Desktop/h/h/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  h
+//
+//  Created by Amiel Kollek on 14-01-21.
+//  Copyright (c) 2014 Amiel Kollek. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
